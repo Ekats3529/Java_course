277eb3d8e5c8c558545d703fd37b48fc835bdbbb
Ekats <suhowa.ekaterina2013@gmail.com>
2023-10-22 00:33:46 +0400

    Add Task 1, Task2, Task3, Task 4 and tests for them

---
+++ src/main/java/edu/hw2/Task1/Expr.java
@@ -0,0 +1,62 @@
+package edu.hw2.Task1;
+
+public sealed interface Expr {
+    double evaluate();
+
+    public record Constant(double digit) implements Expr {
+        @Override
+        public double evaluate() {
+            return digit;
+        }
+    }
+
+    public record Negate(double digit) implements Expr {
+        public Negate(Expr expr){
+            this(expr.evaluate());
+        }
+        @Override
+        public double evaluate() {
+            return -digit;
+        }
+    }
+
+    public record Exponent(double digit, int pow) implements Expr {
+        public Exponent(Expr expr, int pow){
+            this(expr.evaluate(), pow);
+        }
+        @Override
+        public double evaluate() {
+            double result = 1;
+            if (pow > 0) {
+                for (int i = 1; i <= pow; i++) {
+                    result *= digit;
+                }
+            } else {
+                for (int i = 0; i > pow; i--) {
+                    result /= digit;
+                }
+            }
+            return result;
+        }
+    }
+
+    public record Addition(double digit1, double digit2) implements Expr {
+        public Addition(Expr expr1, Expr expr2){
+            this(expr1.evaluate(), expr2.evaluate());
+        }
+        @Override
+        public double evaluate() {
+            return digit1 + digit2;
+        }
+    }
+
+    public record Multiplication(double digit1, double digit2) implements Expr {
+        public Multiplication(Expr expr1, Expr expr2){
+            this(expr1.evaluate(), expr2.evaluate());
+        }
+        @Override
+        public double evaluate() {
+            return digit1 * digit2;
+        }
+    }
+}
---
+++ src/test/java/edu/hw2/TestForTask1.java
@@ -0,0 +1,43 @@
+package edu.hw2;
+
+import edu.hw2.Task1.Expr;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestForTask1 {
+    @Test
+    public final void test1() {
+
+        var two = new Expr.Constant(2);
+        var four = new Expr.Constant(4);
+        var negOne = new Expr.Negate(new Expr.Constant(1));
+        var sumTwoFour = new Expr.Addition(two, four);
+        var mult = new Expr.Multiplication(sumTwoFour, negOne);
+        var exp = new Expr.Exponent(mult, 2);
+        var res = new Expr.Addition(exp, new Expr.Constant(1));
+
+        double expectedResult = 37.0;
+
+        assertEquals(expectedResult, res.evaluate());
+    }
+
+    @Test
+    public final void test2() {
+
+        double expectedResult = 1;
+        var exp = new Expr.Exponent(6, 0);
+
+        assertEquals(expectedResult, exp.evaluate());
+    }
+
+    @Test
+    public final void test3() {
+
+        double expectedResult = 1. / 36;
+        var exp = new Expr.Exponent(6, -2);
+
+        assertEquals(expectedResult, exp.evaluate());
+    }
+
+
+}
