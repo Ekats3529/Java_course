14a1fc955da551d3faeb676eb18bd19ddfd672fb
Ekats <suhowa.ekaterina2013@gmail.com>
2023-11-09 23:53:42 +0400

    Task 7-8 and tests

---
+++ src/main/java/edu/hw5/Task7.java
@@ -0,0 +1,15 @@
+package edu.hw5;
+
+public class Task7 {
+    public static boolean containsAtLeastThreeSymbolsAndTheThirdIsZero(String str){
+        return str.matches("^[01]{2}0[01]*$");
+    }
+
+    public static boolean startsWithAndEndsWithTheSameSymbol(String str){
+        return str.matches("^([01]).*\\1$");
+    }
+
+    public static boolean lengthBetweenOneAndThree(String str){
+        return str.matches("^[01]{1,3}$");
+    }
+}
---
+++ src/main/java/edu/hw5/Task8.java
@@ -0,0 +1,36 @@
+package edu.hw5;
+
+public class Task8 {
+    /* Напишите регулярные выражения для строк из алфавита {0, 1} */
+    /* нечетной длины */
+    public static boolean isLengthOdd(String str) {
+        return str.matches("^[01]([01]{2})*$");
+    }
+    /* начинается с 0 и имеет нечетную длину, или начинается с 1 и имеет четную длину */
+    public static boolean startsWithZeroLengthEvenOrStartsWithOneLengthOdd(String str) {
+        return str.matches("^0([01]{2})*$|^1[01]([01]{2})*$");
+    }
+    /* количество 0 кратно 3 */
+    public static boolean countOfZeroMultipleOfThree(String str) {
+        return false;
+    }
+    /* любая строка, кроме 11 или 111 */
+    public static boolean notTwoOrThreeOnes(String str) {
+        return str.matches("^(?!(11$|111$))[01]*$");
+    }
+    /* каждый нечетный символ равен 1 */
+    public static boolean everyOddIsOne(String str) {
+        return str.matches("^1([01]1)*[01]?$");
+    }
+
+    /* содержит не менее двух 0 и не более одной 1 */
+    public static boolean containsAtLeastTwoZeroAndSingleOrNoOne(String str) {
+        return str.matches("^0*(010|100|001|00)0*$");
+    }
+
+    /* нет последовательных 1 */
+    public static boolean noConcurrentOnes(String str) {
+        return str.matches("^(0|10)*$");
+    }
+
+}
