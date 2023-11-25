3cdbf35ef53b7478ec7e4e995a2e241402c3f1ae
Ekats <suhowa.ekaterina2013@gmail.com>
2023-11-25 07:11:06 +0400

    Task1 + Task2

---
+++ src/main/java/edu/hw7/Task3/ReadWriteLockPersonDatabase.java
@@ -0,0 +1,30 @@
+package edu.hw7.Task3;
+
+import java.util.List;
+
+public class ReadWriteLockPersonDatabase implements PersonDatabase{
+    @Override
+    public void add(Person person) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Person> findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<Person> findByAddress(String address) {
+        return null;
+    }
+
+    @Override
+    public List<Person> findByPhone(String phone) {
+        return null;
+    }
+}
